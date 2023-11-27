bbbeb2d6ecdb55046710a08a4f39b63a039dcdb9
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-11-27 20:10:05 +0300

    Add files via upload

---
+++ ödev2-1473/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev2_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dongu_say = 1;
+            int toplam = 0;
+            while (toplam < 1000)
+            {
+                toplam = toplam + dongu_say;
+                dongu_say += 1;
+            }
+            MessageBox.Show ("döngu toplam"+ dongu_say .ToString()+"kez çaliştir");
+            
+
+        }
+    }
+}
---
+++ ödev2-1473/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev2_1473
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
